6a485089c49f24181c5a6a0ce625e7b1fca685a6
Kleak <me@kleak.dev>
2021-01-17 19:49:32 +0100

    fix deploy

--- migrations/2_first_deploy.js
+++ migrations/2_first_deploy.js
@@ -4,7 +4,7 @@ const SharedAccountManager = artifacts.require('SharedAccountManager');
 module.exports = async (deployer, network, accounts) => {
     if (network == 'development') {
         await deployer.deploy(FakeDai);
+        await deployer.deploy(SharedAccountManager, '0x88bf67b3c033B0b46E33bCcAc0382Aa628fCfe01');
     }
 
-    await deployer.deploy(SharedAccountManager);
 }
