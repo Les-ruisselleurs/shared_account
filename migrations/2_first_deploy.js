e3aba483176f9b19edbe079332fe61d424edf31f
Kleak <me@kleak.dev>
2021-01-17 19:54:40 +0100

    add deployement for smelly-riddle

--- migrations/2_first_deploy.js
+++ migrations/2_first_deploy.js
@@ -5,6 +5,8 @@ module.exports = async (deployer, network, accounts) => {
     if (network == 'development') {
         await deployer.deploy(FakeDai);
         await deployer.deploy(SharedAccountManager, '0x88bf67b3c033B0b46E33bCcAc0382Aa628fCfe01');
+    } else if (network == 'smelly-riddle') {
+        await deployer.deploy(FakeDai);
+        await deployer.deploy(SharedAccountManager, '0x88bf67b3c033B0b46E33bCcAc0382Aa628fCfe01');
     }
-
 }
